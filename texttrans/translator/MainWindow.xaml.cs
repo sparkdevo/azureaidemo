8173087f7e3caf96dba0c8f2992210a214fdb2eb
nickli <nick.li@grapecity.com>
2018-04-04 10:46:09 +0800

    add text translator project

---
+++ texttrans/translator/MainWindow.xaml.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace translator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Translator text subscription key from Microsoft Azure dashboard
+        const string TEXT_TRANSLATION_API_SUBSCRIPTION_KEY = "757562e0fcd147eea4602aa8cbfa506a";
+        const string TEXT_ANALYTICS_API_SUBSCRIPTION_KEY = "d3837cc8306541c19d3f4f792116a0b6";
+        const string BING_SPELL_CHECK_API_SUBSCRIPTION_KEY = "d3d0b46a3bab47fb95e0cbcaacc52e80";
+
+        const string TEXT_TRANSLATION_API_ENDPOINT = "https://api.microsofttranslator.com/v2/Http.svc/";
+        const string TEXT_ANALYTICS_API_ENDPOINT = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/";
+        const string BING_SPELL_CHECK_API_ENDPOINT = "https://api.cognitive.microsoft.com/bing/v7.0/spellcheck/";
+
+        private string[] languageCodes;     // array of language codes
+
+        // Dictionary to map language code from friendly name (sorted case-insensitively on language name)
+        private SortedDictionary<string, string> languageCodesAndTitles =
+            new SortedDictionary<string, string>(Comparer<string>.Create((a, b) => string.Compare(a, b, true)));
+
+        public MainWindow()
+        {
+            // at least show an error dialog when we get an unexpected error
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleExceptions);
+
+            if (TEXT_TRANSLATION_API_SUBSCRIPTION_KEY.Length != 32
+                || TEXT_ANALYTICS_API_SUBSCRIPTION_KEY.Length != 32
+                || BING_SPELL_CHECK_API_SUBSCRIPTION_KEY.Length != 32)
+            {
+                MessageBox.Show("One or more invalid API subscription keys.\n\n" +
+                    "Put your keys in the *_API_SUBSCRIPTION_KEY variables in MainWindow.xaml.cs.",
+                    "Invalid Subscription Key(s)", MessageBoxButton.OK, MessageBoxImage.Error);
+                System.Windows.Application.Current.Shutdown();
+            }
+            else
+            {
+                InitializeComponent();          // start the GUI
+                GetLanguagesForTranslate();     // get codes of languages that can be translated
+                GetLanguageNames();             // get friendly names of languages
+                PopulateLanguageMenus();        // fill the drop-down language lists
+            }
+        }
+
+        // Global exception handler to display error message and exit
+        private static void HandleExceptions(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            MessageBox.Show("Caught " + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        // ***** POPULATE LANGUAGE MENUS
+        private void PopulateLanguageMenus()
+        {
+            // Add option to automatically detect the source language
+            FromLanguageComboBox.Items.Add("Detect");
+
+            int count = languageCodesAndTitles.Count;
+            foreach (string menuItem in languageCodesAndTitles.Keys)
+            {
+                FromLanguageComboBox.Items.Add(menuItem);
+                ToLanguageComboBox.Items.Add(menuItem);
+            }
+
+            // set default languages
+            FromLanguageComboBox.SelectedItem = "Detect";
+            ToLanguageComboBox.SelectedItem = "English";
+        }
+
+        // ***** DETECT LANGUAGE OF TEXT TO BE TRANSLATED
+        private string DetectLanguage(string text)
+        {
+            string uri = TEXT_ANALYTICS_API_ENDPOINT + "languages?numberOfLanguagesToDetect=1";
+
+            // create request to Text Analytics API
+            HttpWebRequest detectLanguageWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            detectLanguageWebRequest.Headers.Add("Ocp-Apim-Subscription-Key", TEXT_ANALYTICS_API_SUBSCRIPTION_KEY);
+            detectLanguageWebRequest.Method = "POST";
+
+            // create and send body of request
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            string jsonText = serializer.Serialize(text);
+
+            string body = "{ \"documents\": [ { \"id\": \"0\", \"text\": " + jsonText + "} ] }";
+            byte[] data = Encoding.UTF8.GetBytes(body);
+            detectLanguageWebRequest.ContentLength = data.Length;
+
+            using (var requestStream = detectLanguageWebRequest.GetRequestStream())
+                requestStream.Write(data, 0, data.Length);
+
+            HttpWebResponse response = (HttpWebResponse)detectLanguageWebRequest.GetResponse();
+
+            // read and and parse JSON response
+            var responseStream = response.GetResponseStream();
+            var jsonString = new StreamReader(responseStream, Encoding.GetEncoding("utf-8")).ReadToEnd();
+            dynamic jsonResponse = serializer.DeserializeObject(jsonString);
+
+            // fish out the detected language code
+            var languageInfo = jsonResponse["documents"][0]["detectedLanguages"][0];
+            if (languageInfo["score"] > (decimal)0.5)
+                return languageInfo["iso6391Name"];
+            else
+                return "";
+        }
+
+        // ***** CORRECT SPELLING OF TEXT TO BE TRANSLATED
+        private string CorrectSpelling(string text)
+        {
+            string uri = BING_SPELL_CHECK_API_ENDPOINT + "?mode=spell&mkt=en-US";
+
+            // create request to Bing Spell Check API
+            HttpWebRequest spellCheckWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            spellCheckWebRequest.Headers.Add("Ocp-Apim-Subscription-Key", BING_SPELL_CHECK_API_SUBSCRIPTION_KEY);
+            spellCheckWebRequest.Method = "POST";
+            spellCheckWebRequest.ContentType = "application/x-www-form-urlencoded"; // doesn't work without this
+
+            // create and send body of request
+            string body = "text=" + System.Web.HttpUtility.UrlEncode(text);
+            byte[] data = Encoding.UTF8.GetBytes(body);
+            spellCheckWebRequest.ContentLength = data.Length;
+            using (var requestStream = spellCheckWebRequest.GetRequestStream())
+                requestStream.Write(data, 0, data.Length);
+            HttpWebResponse response = (HttpWebResponse)spellCheckWebRequest.GetResponse();
+
+            // read and parse JSON response and get spelling corrections
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            var responseStream = response.GetResponseStream();
+            var jsonString = new StreamReader(responseStream, Encoding.GetEncoding("utf-8")).ReadToEnd();
+            dynamic jsonResponse = serializer.DeserializeObject(jsonString);
+            var flaggedTokens = jsonResponse["flaggedTokens"];
+
+            // construct sorted dictionary of corrections in reverse order in string (right to left)
+            // so that making a correction can't affect later indexes
+            var corrections = new SortedDictionary<int, string[]>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+            for (int i = 0; i < flaggedTokens.Length; i++)
+            {
+                var correction = flaggedTokens[i];
+                var suggestion = correction["suggestions"][0];  // consider only first suggestion
+                if (suggestion["score"] > (decimal)0.7)         // take it only if highly confident
+                    corrections[(int)correction["offset"]] = new string[]   // dict key   = offset
+                        { correction["token"], suggestion["suggestion"] };  // dict value = {error, correction}
+            }
+
+            // apply the corrections in order from right to left
+            foreach (int i in corrections.Keys)
+            {
+                var oldtext = corrections[i][0];
+                var newtext = corrections[i][1];
+
+                // apply capitalization from original text to correction - all caps or initial caps
+                if (text.Substring(i, oldtext.Length).All(char.IsUpper)) newtext = newtext.ToUpper();
+                else if (char.IsUpper(text[i])) newtext = newtext[0].ToString().ToUpper() + newtext.Substring(1);
+
+                text = text.Substring(0, i) + newtext + text.Substring(i + oldtext.Length);
+            }
+
+            return text;
+        }
+
+        // ***** GET TRANSLATABLE LANGAUGE CODES
+        private void GetLanguagesForTranslate()
+        {
+            // send request to get supported language codes
+            string uri = TEXT_TRANSLATION_API_ENDPOINT + "GetLanguagesForTranslate?scope=text";
+            WebRequest WebRequest = WebRequest.Create(uri);
+            WebRequest.Headers.Add("Ocp-Apim-Subscription-Key", TEXT_TRANSLATION_API_SUBSCRIPTION_KEY);
+            WebResponse response = null;
+
+            // read and parse the XML response
+            response = WebRequest.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                System.Runtime.Serialization.DataContractSerializer dcs =
+                    new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
+                List<string> languagesForTranslate = (List<string>)dcs.ReadObject(stream);
+                languageCodes = languagesForTranslate.ToArray();
+            }
+        }
+
+        //***** GET FRIENDLY LANGUAGE NAMES
+        private void GetLanguageNames()
+        {
+            // send request to get supported language names in English
+            string uri = TEXT_TRANSLATION_API_ENDPOINT + "GetLanguageNames?locale=en";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Headers.Add("Ocp-Apim-Subscription-Key", TEXT_TRANSLATION_API_SUBSCRIPTION_KEY);
+            request.ContentType = "text/xml";
+            request.Method = "POST";
+            System.Runtime.Serialization.DataContractSerializer dcs =
+                new System.Runtime.Serialization.DataContractSerializer(Type.GetType("System.String[]"));
+            using (System.IO.Stream stream = request.GetRequestStream())
+                dcs.WriteObject(stream, languageCodes);
+
+            // read and parse the XML response
+            var response = request.GetResponse();
+            string[] languageNames;
+            using (Stream stream = response.GetResponseStream())
+                languageNames = (string[])dcs.ReadObject(stream);
+
+            //load the dictionary for the combo box
+            for (int i = 0; i < languageNames.Length; i++)
+                languageCodesAndTitles.Add(languageNames[i], languageCodes[i]);
+        }
+
+        // ***** PERFORM TRANSLATION ON BUTTON CLICK
+        private void TranslateButton_Click(object sender, EventArgs e)
+        {
+            string textToTranslate = TextToTranslate.Text.Trim();
+
+            string fromLanguage = FromLanguageComboBox.SelectedValue.ToString();
+            string fromLanguageCode;
+
+            // auto-detect source language if requested
+            if (fromLanguage == "Detect")
+            {
+                fromLanguageCode = DetectLanguage(textToTranslate);
+                if (!languageCodes.Contains(fromLanguageCode))
+                {
+                    MessageBox.Show("The source language could not be detected automatically " +
+                        "or is not supported for translation.", "Language detection failed",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            else
+                fromLanguageCode = languageCodesAndTitles[fromLanguage];
+
+            string toLanguageCode = languageCodesAndTitles[ToLanguageComboBox.SelectedValue.ToString()];
+
+            // spell-check the source text if the source language is English
+            if (fromLanguageCode == "en")
+            {
+                if (textToTranslate.StartsWith("-"))    // don't spell check in this case
+                    textToTranslate = textToTranslate.Substring(1);
+                else
+                {
+                    textToTranslate = CorrectSpelling(textToTranslate);
+                    TextToTranslate.Text = textToTranslate;     // put corrected text into input field
+                }
+            }
+
+            // handle null operations: no text or same source/target languages
+            if (textToTranslate == "" || fromLanguageCode == toLanguageCode)
+            {
+                TranslatedTextLabel.Content = textToTranslate;
+                return;
+            }
+
+            // send HTTP request to perform the translation
+            string uri = string.Format(TEXT_TRANSLATION_API_ENDPOINT + "Translate?text=" +
+                System.Web.HttpUtility.UrlEncode(textToTranslate) + "&from={0}&to={1}", fromLanguageCode, toLanguageCode);
+            var translationWebRequest = HttpWebRequest.Create(uri);
+            translationWebRequest.Headers.Add("Ocp-Apim-Subscription-Key", TEXT_TRANSLATION_API_SUBSCRIPTION_KEY);
+            WebResponse response = null;
+            response = translationWebRequest.GetResponse();
+
+            // Parse the response XML
+            Stream stream = response.GetResponseStream();
+            StreamReader translatedStream = new StreamReader(stream, Encoding.GetEncoding("utf-8"));
+            System.Xml.XmlDocument xmlResponse = new System.Xml.XmlDocument();
+            xmlResponse.LoadXml(translatedStream.ReadToEnd());
+
+            // Update the translation field
+            TranslatedTextLabel.Content = xmlResponse.InnerText;
+        }
+    }
+}
