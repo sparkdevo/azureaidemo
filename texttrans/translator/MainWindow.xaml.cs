1eb54d4ad0359e6facbfa1e7ac124b64d774b452
nickli <nick.li@grapecity.com>
2018-04-07 15:47:54 +0800

    change label to a readonly textbox.

--- texttrans/translator/MainWindow.xaml.cs
+++ texttrans/translator/MainWindow.xaml.cs
@@ -16,11 +16,9 @@ namespace translator
     {
         // Translator text subscription key from Microsoft Azure dashboard
         const string TEXT_TRANSLATION_API_SUBSCRIPTION_KEY = "757562e0fcd147eea4602aa8cbfa506a";
-        const string TEXT_ANALYTICS_API_SUBSCRIPTION_KEY = "d3837cc8306541c19d3f4f792116a0b6";
         const string BING_SPELL_CHECK_API_SUBSCRIPTION_KEY = "d3d0b46a3bab47fb95e0cbcaacc52e80";
 
         const string TEXT_TRANSLATION_API_ENDPOINT = "https://api.microsofttranslator.com/v2/Http.svc/";
-        const string TEXT_ANALYTICS_API_ENDPOINT = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/";
         const string BING_SPELL_CHECK_API_ENDPOINT = "https://api.cognitive.microsoft.com/bing/v7.0/spellcheck/";
 
         private string[] languageCodes;     // array of language codes
@@ -35,7 +33,6 @@ namespace translator
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleExceptions);
 
             if (TEXT_TRANSLATION_API_SUBSCRIPTION_KEY.Length != 32
-                || TEXT_ANALYTICS_API_SUBSCRIPTION_KEY.Length != 32
                 || BING_SPELL_CHECK_API_SUBSCRIPTION_KEY.Length != 32)
             {
                 MessageBox.Show("One or more invalid API subscription keys.\n\n" +
@@ -77,43 +74,6 @@ namespace translator
             FromLanguageComboBox.SelectedItem = "英语";
             ToLanguageComboBox.SelectedItem = "简体中文";
         }
-
-        // ***** DETECT LANGUAGE OF TEXT TO BE TRANSLATED
-        //private string DetectLanguage(string text)
-        //{
-        //    string uri = TEXT_ANALYTICS_API_ENDPOINT + "languages?numberOfLanguagesToDetect=1";
-
-        //    // create request to Text Analytics API
-        //    HttpWebRequest detectLanguageWebRequest = (HttpWebRequest)WebRequest.Create(uri);
-        //    detectLanguageWebRequest.Headers.Add("Ocp-Apim-Subscription-Key", TEXT_ANALYTICS_API_SUBSCRIPTION_KEY);
-        //    detectLanguageWebRequest.Method = "POST";
-
-        //    // create and send body of request
-        //    var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-        //    string jsonText = serializer.Serialize(text);
-
-        //    string body = "{ \"documents\": [ { \"id\": \"0\", \"text\": " + jsonText + "} ] }";
-        //    byte[] data = Encoding.UTF8.GetBytes(body);
-        //    detectLanguageWebRequest.ContentLength = data.Length;
-
-        //    using (var requestStream = detectLanguageWebRequest.GetRequestStream())
-        //        requestStream.Write(data, 0, data.Length);
-
-        //    // 这个调用有问题，总是返回401
-        //    HttpWebResponse response = (HttpWebResponse)detectLanguageWebRequest.GetResponse();
-
-        //    // read and and parse JSON response
-        //    var responseStream = response.GetResponseStream();
-        //    var jsonString = new StreamReader(responseStream, Encoding.GetEncoding("utf-8")).ReadToEnd();
-        //    dynamic jsonResponse = serializer.DeserializeObject(jsonString);
-
-        //    // fish out the detected language code
-        //    var languageInfo = jsonResponse["documents"][0]["detectedLanguages"][0];
-        //    if (languageInfo["score"] > (decimal)0.5)
-        //        return languageInfo["iso6391Name"];
-        //    else
-        //        return "";
-        //}
 
         // ***** CORRECT SPELLING OF TEXT TO BE TRANSLATED
         private string CorrectSpelling(string text)
@@ -220,19 +180,6 @@ namespace translator
             string fromLanguage = FromLanguageComboBox.SelectedValue.ToString();
             string fromLanguageCode;
 
-            // auto-detect source language if requested
-            //if (fromLanguage == "自动检测")//Detect
-            //{
-            //    fromLanguageCode = DetectLanguage(textToTranslate);
-            //    if (!languageCodes.Contains(fromLanguageCode))
-            //    {
-            //        MessageBox.Show("The source language could not be detected automatically " +
-            //            "or is not supported for translation.", "Language detection failed",
-            //            MessageBoxButton.OK, MessageBoxImage.Error);
-            //        return;
-            //    }
-            //}
-            //else
             fromLanguageCode = languageCodesAndTitles[fromLanguage];
 
             string toLanguageCode = languageCodesAndTitles[ToLanguageComboBox.SelectedValue.ToString()];
@@ -252,7 +199,7 @@ namespace translator
             // handle null operations: no text or same source/target languages
             if (textToTranslate == "" || fromLanguageCode == toLanguageCode)
             {
-                TranslatedTextLabel.Content = textToTranslate;
+                TranslatedText.Text = textToTranslate;
                 return;
             }
 
@@ -271,7 +218,7 @@ namespace translator
             xmlResponse.LoadXml(translatedStream.ReadToEnd());
 
             // Update the translation field
-            TranslatedTextLabel.Content = xmlResponse.InnerText;
+            TranslatedText.Text = xmlResponse.InnerText;
         }
     }
 }
